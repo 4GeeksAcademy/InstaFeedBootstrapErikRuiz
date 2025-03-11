559ef9ed0b3c87f2ecf50c2a52cda790fe8f6136
fuke199131 <erikruiz13@gmail.com>
2025-03-11 14:32:25 +0000

    ProyectoActualizado

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function setGrid(columns) {
-    const gallery = document.getElementById("gallery");
-    gallery.className = columns === 3 ? "grid-container grid-3" : "grid-container grid-1";
-}
-
 function openModal() {
-    document.getElementById("postModal").style.display = "flex";
+    document.getElementById('postModal').style.display = 'block';
 }
-
 function closeModal() {
-    document.getElementById("postModal").style.display = "none";
+    document.getElementById('postModal').style.display = 'none';
+}
+function setGrid(columns) {
+    const gallery = document.getElementById('gallery');
+    gallery.className = 'grid-container grid-' + columns;
+}
+window.onclick = function(event) {
+    if (event.target == document.getElementById('postModal')) {
+        closeModal();
+    }
 }
